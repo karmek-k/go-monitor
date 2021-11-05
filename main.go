6dd21ca56b554a2baa1a3237592da5aac072bd18
Bartosz Gleń <33288445+karmek-k@users.noreply.github.com>
2021-11-05 22:27:26 +0100

    Make utils.GetAccounts also return an error

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 
 	"github.com/karmek-k/go-monitor/routes"
@@ -8,9 +10,14 @@ import (
 )
 
 func main() {
+	accounts, err := utils.GetAccounts()
+	if err != nil {
+		log.Fatalf("An error has occurred while fetching accounts: %v\n", err)
+	}
+
 	r := gin.New()
 
-	r.Use(gin.BasicAuth(utils.GetAccounts()))
+	r.Use(gin.BasicAuth(*accounts))
 
 	r.GET("/memory", routes.MemoryRoute)
 	r.GET("/cpu", routes.CpuRoute)
